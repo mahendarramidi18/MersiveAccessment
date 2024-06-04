e11066fe70eeb586db60832aaa8f08214628cc23
mahendarramidi18 <mahendarramidi18@users.noreply.github.com>
2024-06-04 13:33:34 -0400

    Initial commit

---
+++ src/tree/index.tsx
@@ -0,0 +1,20 @@
+import "./index.css";
+
+const Tree = () => {
+  return (
+    <div className="tree">
+      root
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;ant <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;bear <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant{" "}
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;frog <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dolphin <br />
+    </div>
+  );
+};
+
+export default Tree;
